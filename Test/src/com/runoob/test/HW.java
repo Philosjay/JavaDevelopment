0e7800b228e9c2618d1b7cac92a090042628c07d
Philosjay <305546044@qq.com>
2017-10-25 00:32:35 +0800

    first commit

---
+++ Test/src/com/runoob/test/HW.java
@@ -0,0 +1,10 @@
+package com.runoob.test;
+
+public class HW {
+	   public static void main(String args[]){
+		      char[] helloArray = { 'r', 'u', 'n', 'o', 'o', 'b'};
+		      String helloString = new String(helloArray);  
+		      System.out.println( helloString );
+		      System.out.println("Oh my!");
+		   }
+}
